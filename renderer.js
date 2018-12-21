84436630bcc435bc6a25d78e8b99998dbe1c0ccf
burningTyger <b6tyger@gmail.com>
2018-12-21 09:37:15 +0100

    webview.printToPdf() bug

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const fs = require('fs')
+
+onload = () => {
+  const webview = document.querySelector('webview')
+
+  const loadstop = () => {
+    console.log(webview)
+    webview.printToPDF({}, (error, data) => {
+      console.log(data, error)
+      if (error) throw error
+      fs.writeFile('output.pdf', data, error => {
+        if (error) throw error
+      })
+    })
+  }
+
+  webview.addEventListener('did-stop-loading', loadstop)
+}
